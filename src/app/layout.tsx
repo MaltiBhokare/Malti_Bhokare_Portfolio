39b9a5a3e4f35123b27391458481ec7d9eef5d70
Malti <maltibhokare2003@gmail.com>
2025-09-16 22:14:48 +0530

    fix: add Tailwind config and update layout for dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,32 @@
+// import "./globals.css";
+// import Navbar from "@/components/Navbar";
+// import Footer from "@/components/Footer";
+
+// export const metadata = {
+//   title: "Malti Bhokare | Portfolio",
+//   description: "MERN Stack Developer | AI Enthusiast | Computer Engineer",
+//   icons: {
+//     icon: "/favicon.png", // 👈 put your favicon path here
+//   },
+// };
+
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <html lang="en">
+//       <body className="bg-white text-black font-sans transition-colors duration-300">
+//         <Navbar />
+//         <main className="container mx-auto px-6 py-12">{children}</main>
+//         <Footer />
+//       </body>
+//     </html>
+//   );
+// }
+
+
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
@@ -16,8 +45,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className="bg-white text-black font-sans transition-colors duration-300">
+    <html lang="en" className="dark"> 
+      <body className="bg-black text-white font-sans transition-colors duration-300">
         <Navbar />
         <main className="container mx-auto px-6 py-12">{children}</main>
         <Footer />
